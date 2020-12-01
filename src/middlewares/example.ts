9113c81a689e24390504a21d0e8836f5a7eba64c
Jérémie Parker <github@jeremie-parker.com>
2020-12-01 13:37:04 +0100

    doc(example): add a link to @p-j/eapi-middleware-headers

--- src/middlewares/example.ts
+++ src/middlewares/example.ts
@@ -2,6 +2,8 @@
  * This is an example middleware to showcase how to build reusable & configurable behaviors for
  * your request handlers
  * You can find some useful middlewares here: https://github.com/p-j/eapi
+ * A more involved version of this middleware is available here: https://github.com/p-j/eapi/tree/main/packages/eapi-middleware-headers
+ * With support for combining/overriding/removing headers both on the Request & the Response
  */
 export function withHeader({ headers = {} }: { headers: { [key: string]: string } }): Middleware {
   return function _withHeader(requestHandler) {
